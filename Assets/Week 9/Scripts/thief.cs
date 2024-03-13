f450500968d9360cb3b1daeb64f8e4157093daf6
luesue <luesue@OA-S143-02>
2024-03-13 16:34:24 -0400

    doing daggers

--- Assets/Week 9/Scripts/thief.cs
+++ Assets/Week 9/Scripts/thief.cs
@@ -1,11 +1,21 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
 public class thief : Villager
 {
+    public GameObject dagger;
+    public Transform spawn;
     public override ChestType CanOpen()
     {
         return ChestType.Thief;
+    }
+    protected override void Attack()
+    {
+        destination = transform.position;
+        base.Attack();
+        Instantiate(dagger, spawn.position, spawn.rotation);
+        Instantiate(dagger, spawn.position, spawn.rotation);
     }
 }
