9f3780f569e71112a03e522ccfe5a7f5aca48426
luesue <luesue@OA-S143-02>
2024-03-13 17:35:05 -0400

    done works

--- Assets/Week 9/Scripts/CharacterControl.cs
+++ Assets/Week 9/Scripts/CharacterControl.cs
@@ -1,11 +1,18 @@
 using System;
 using System.Collections;
 using System.Collections.Generic;
+using TMPro;
+using Unity.VisualScripting;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class CharacterControl : MonoBehaviour
 {
+    public TextMeshProUGUI className;
+    public ChestType type; 
     public static Villager SelectedVillager { get; private set; }
+    public static string namey;
+
     public static void SetSelectedVillager(Villager villager)
     {
         if(SelectedVillager != null)
@@ -14,6 +21,16 @@ public class CharacterControl : MonoBehaviour
         }
         SelectedVillager = villager;
         SelectedVillager.Selected(true);
+        namey = villager.name;
     }
-    
+    private void Start()
+    {
+        namey = "Not Selected";
+    }
+    private void Update()
+    {
+        className.text = namey;
+
+    }
+
 }
