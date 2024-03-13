1b237b6230c3eb67c395e12b85ea922ed17d304a
luesue <luesue@OA-S141-14>
2024-03-13 13:56:52 -0400

    just getting github

---
+++ Assets/Week 9/Scripts/archer.cs
@@ -0,0 +1,9 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class archer : Villager
+{
+    public GameObject arrowFAB;
+    public Transform spawn;
+}
