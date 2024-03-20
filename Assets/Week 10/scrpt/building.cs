20a06e203aec4382885e87c6afd2656f823439e9
luesue <luesue@OA-S143-05>
2024-03-20 17:17:43 -0400

    start

--- Assets/Week 10/scrpt/building.cs
+++ Assets/Week 10/scrpt/building.cs
@@ -12,21 +12,81 @@ public class building : MonoBehaviour
     public SpriteRenderer stand;
     public SpriteRenderer vase;
     public SpriteRenderer top;
-    float timer;
+    float timer=0;
+    
 
     // Start is called before the first frame update
     void Start()
     {
+
         //call coroutine heere
         StartCoroutine(Build());
     }
     IEnumerator Build()
     {
-        timer += Time.deltaTime;
-        if (timer < 1) {
-            table.transform.localScale = Vector3.Lerp(transform.localScale, transform.localScale * 2, timer);
+        table.transform.localScale = Vector3.zero;
+        chair1.transform.localScale = Vector3.zero;
+        chair2.transform.localScale = Vector3.zero;
+        lamp.transform.localScale = Vector3.zero;
+        stand.transform.localScale = Vector3.zero;
+        vase.transform.localScale = Vector3.zero;
+        top.transform.localScale = Vector3.zero;
+
+        while ( table.transform.localScale.z < 1) {
+            timer += Time.deltaTime;
+            table.transform.localScale = Vector3.Lerp(Vector3.zero, Vector3.one, timer);
+            yield return null;
         }
-        yield return new WaitForSeconds(2);
+        yield return new WaitForSeconds(0.5f);
+        timer = 0;
+        while (chair1.transform.localScale.z < 1)
+        {
+            timer += Time.deltaTime;
+            chair1.transform.localScale = Vector3.Lerp(Vector3.zero, Vector3.one, timer);
+            yield return null;
+        }
+        yield return new WaitForSeconds(0.5f);
+        timer = 0;
+        while (chair2.transform.localScale.z < 1)
+        {
+            timer += Time.deltaTime;
+            chair2.transform.localScale = Vector3.Lerp(Vector3.zero, Vector3.one, timer);
+            yield return null;
+        }
+        yield return new WaitForSeconds(0.5f);
+        timer = 0;
+        while (lamp.transform.localScale.z < 1)
+        {
+            timer += Time.deltaTime;
+            lamp.transform.localScale = Vector3.Lerp(Vector3.zero, Vector3.one, timer);
+            yield return null;
+        }
+        yield return new WaitForSeconds(0.5f);
+        timer = 0;
+        while (vase.transform.localScale.z < 1)
+        {
+            timer += Time.deltaTime;
+            vase.transform.localScale = Vector3.Lerp(Vector3.zero, Vector3.one, timer);
+            yield return null;
+        }
+        yield return new WaitForSeconds(0.5f);
+        timer = 0;
+        while (stand.transform.localScale.z < 1)
+        {
+            timer += Time.deltaTime;
+            stand.transform.localScale = Vector3.Lerp(Vector3.zero, Vector3.one, timer);
+            yield return null;
+        }
+        yield return new WaitForSeconds(0.5f);
+        timer = 0;
+        while (top.transform.localScale.z < 2)
+        {
+            timer += Time.deltaTime;
+            top.transform.localScale = Vector3.Lerp(Vector3.zero, Vector3.one, timer);
+            yield return null;
+        }
+        yield return new WaitForSeconds(0.5f);
+        timer = 0;
     }
     // Update is called once per frame
     void Update()
