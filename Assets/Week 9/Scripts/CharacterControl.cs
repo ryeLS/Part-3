f8ed2709e969ab5e19044d36ed6c9a67b56a3553
luesue <luesue@OA-S141-14>
2024-03-20 14:02:46 -0400

    almost done

--- Assets/Week 9/Scripts/CharacterControl.cs
+++ Assets/Week 9/Scripts/CharacterControl.cs
@@ -9,7 +9,8 @@ using UnityEngine.UI;
 public class CharacterControl : MonoBehaviour
 {
     public TextMeshProUGUI className;
-    public ChestType type; 
+    public ChestType type;
+    public static CharacterControl Instance;
     public static Villager SelectedVillager { get; private set; }
     public static string namey;
 
@@ -21,16 +22,14 @@ public class CharacterControl : MonoBehaviour
         }
         SelectedVillager = villager;
         SelectedVillager.Selected(true);
-        namey = villager.name;
+        //namey = villager.name;
+        Instance.className.text = villager.ToString();
     }
     private void Start()
     {
-        namey = "Not Selected";
+        //namey = "Not Selected";
+        Instance = this;
     }
-    private void Update()
-    {
-        className.text = namey;
 
-    }
 
 }
--- Assets/Week 9/Scripts/thief.cs
+++ Assets/Week 9/Scripts/thief.cs
@@ -1,22 +1,56 @@
 using System;
 using System.Collections;
 using System.Collections.Generic;
+using UnityEditor;
 using UnityEngine;
 
 public class thief : Villager
 {
     public GameObject dagger;
     public Transform spawn;
+    public float Tspeed = 7;
+    //public float timer;
+    //public float dashtime = 1;
+    //bool isDash;
     public override ChestType CanOpen()
     {
         return ChestType.Thief;
     }
     protected override void Attack()
     {
-        base.Attack();
-        Instantiate(dagger, spawn.position, spawn.rotation);
-        Instantiate(dagger, spawn.position, spawn.rotation);
-        destination = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+        StartCoroutine(dash());
 
+    }
+    IEnumerator dash()
+    {
+        destination = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+        Tspeed = 7;
+        while (Tspeed>3 )
+        {
+            yield return null;
+        }
+        //timer -= Time.deltaTime;
+        //if(timer < 0)
+        //{
+        //    isDash = false;
+        base.Attack();
+        yield return new WaitForSeconds(0.1f);
+            Instantiate(dagger, spawn.position, spawn.rotation);
+        yield return new WaitForSeconds(0.1f);
+        Instantiate(dagger, spawn.position, spawn.rotation);
+            
+        //}
+    }
+    //protected override void Update()
+    //{
+    //    base.Update();
+    //    if (isDash)
+    //    {
+    //        dash();
+    //    }
+    //}
+    public override string ToString()
+    {
+        return "yo mama";
     }
 }
