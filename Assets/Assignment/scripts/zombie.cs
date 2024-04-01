0c36339e757d081fbe9fe60642e098d04c6f80dc
divyaluesue <luesue@sheridancollege.ca>
2024-04-01 02:52:52 -0400

    setting up

---
+++ Assets/Assignment/scripts/zombie.cs
@@ -0,0 +1,22 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+//References
+//Mini Zombie - https://opengameart.org/content/mini-zombie
+//Zombie - https://opengameart.org/content/zombie-character
+//Player - https://opengameart.org/content/2d-hero-guy-character
+public class zombie : MonoBehaviour
+{
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
