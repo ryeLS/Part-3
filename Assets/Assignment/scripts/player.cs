531f6c99e2b5ba1f68bf8766d53dddb8f71fe0c1
divyaluesue <luesue@sheridancollege.ca>
2024-04-01 20:15:25 -0400

    starting zombie functionality. will continue player

---
+++ Assets/Assignment/scripts/player.cs
@@ -0,0 +1,53 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class player : MonoBehaviour
+{
+    float speed = 3;
+    Rigidbody2D rb;
+    Vector2 destination;
+    Vector2 movement;
+    public GameObject pewpew;
+    public float health = 5;
+    // Start is called before the first frame update
+    void Start()
+    {
+        rb =GetComponent<Rigidbody2D>();
+    }
+    private void FixedUpdate()
+    {
+        movement =destination - (Vector2)transform.position;
+        //flips him so he doesnt look so weird-
+        //I mean flips him in direction hes moving(no need for 2 walk cycles)
+        if(movement.x < 0)
+        {
+            transform.localScale = new Vector3(-1,1,1);
+        }
+        if(movement.x > 0)
+        {
+            transform.localScale = new Vector3(1,1,1);
+        }
+        if(movement.magnitude < 0.1)
+        {
+            movement = Vector2.zero;
+            speed = 3;
+        }
+        if(movement.magnitude < 0.1)
+        {
+            movement = Vector2.zero;
+        }
+        rb.MovePosition(rb.position+movement.normalized*speed *Time.deltaTime);
+    }
+    void Update()
+    {
+        if(Input.GetMouseButtonDown(0)) //if mouse clicked then player move to clicked location
+        {
+            destination = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+        }
+    }
+    public void HitTaken(float damage)
+    {
+        health -= damage;
+    }
+}
--- Assets/Assignment/scripts/zombie.cs
+++ Assets/Assignment/scripts/zombie.cs
@@ -7,16 +7,26 @@ using UnityEngine;
 //Player - https://opengameart.org/content/2d-hero-guy-character
 public class zombie : MonoBehaviour
 {
-
+    public float speed = 3f;
     // Start is called before the first frame update
     void Start()
     {
-        
+        //StartCoroutine(StaggeredSpawn());
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        transform.Translate(Vector3.left * speed * Time.deltaTime);
+        Vector2 screenPosition = Camera.main.WorldToScreenPoint(transform.position);
+        if (screenPosition.x > Screen.height || screenPosition.x < 0)
+        {
+            Debug.Log("haha");
+            Destroy(gameObject);
+        }
+    }
+    public void OnTriggerEnter2D(Collider2D collision)
+    {
+        collision.gameObject.SendMessage("HitTaken", 1, SendMessageOptions.DontRequireReceiver);
     }
 }
